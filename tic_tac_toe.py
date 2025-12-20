460c64ea9c8c1df2e162f62b60f01c293054f5a3
Pratyaksh Sahai <pratyakshsahai@gmail.com>
2025-12-20 13:30:46 +0530

    Added python version of tic-tac-toe

---
+++ tic_tac_toe.py
@@ -0,0 +1,20 @@
+ttt_grid = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
+
+print("-------------")
+for i in range(3):
+  for j in range(3):
+    print("|", ttt_grid[i][j], end=" ")
+  print("|")
+  print("-------------")
+
+def place_XO():
+  pass
+
+def print_ttt():
+  pass
+
+def winner():
+  pass
+
+if __name__ == "__main__":
+  pass
