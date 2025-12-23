30ff1719eb28b029a61a6f008e11495badae8f90
Pratyaksh Sahai <pratyakshsahai@gmail.com>
2025-12-23 20:31:50 +0530

    Updated code logic

--- tic_tac_toe.py
+++ tic_tac_toe.py
@@ -1,18 +1,25 @@
 import time
 
-# place X or O at chosen space and return edited grid
+# place X or O at chosen space and return edited grid if x or o could be placed. else it returns 0 along with appropriate message
 def place_XO(grid, choice, xo):
-  for i in range(3):
-    for j in range(3):
-      if choice == grid[i][j]:
-        if grid[i][j] != 'X' and grid[i][j] != 'O':
-          grid[i][j] == xo
-          break
-        else:
-          print("Position is occupied, try again!")
-          break
-      if choice > 9 or choice < 1:
-          print("Invalid Choice, try again!")
+  
+  positions = { 1: (0,0), 2: (0,1), 3: (0,2),
+                4: (1,0), 5: (1,1), 6: (1,2),
+                7: (2,0), 8: (2,1), 9: (2,2)  }
+
+  if choice in positions.keys():
+    i, j = positions[choice]  # unpacking the tuples that are stored as values corresponding to the choice of key of the positions dictionary 
+    
+    if grid[i][j] != 'X' and grid[i][j] != 'O':
+      grid[i][j] = xo
+    else:
+      print("Position is occupied, try again!")
+      return 0
+  
+  else:
+      print("Invalid Choice, try again!")
+      return 0
+  
   return grid
 
 # print the grid
@@ -25,19 +32,16 @@ def print_grid(grid):
     print("-------------")
 
 # prints the winner if any
-def print_winner(grid):
-  for i in range(3):
-    for j in range(3):
-      if (grid[0][j] == grid[1][j] == grid[2][j]):
-        print(grid[0][j], "is the winner!")
-        break
-      elif (grid[i][0] == grid[i][1] == grid[i][2]):
-        print(grid[i][0], "is the winner!")
-        break
-      elif (grid[0][0] == grid[1][1] == grid[2][2]):
-        print(grid[0][0], "is the winner!")
-      else:
-        print("It's a DRAW!")
+def winner(grid):
+  for x in range(3):
+    if (grid[0][x] == grid[1][x] == grid[2][x]):
+      return grid[0][x]
+    elif (grid[x][0] == grid[x][1] == grid[x][2]):
+      return grid[x][0]
+    elif (grid[0][0] == grid[1][1] == grid[2][2]):
+      return grid[0][0]
+    else:
+      return 0
 
 def main():
 
@@ -55,31 +59,42 @@ def main():
   # Print the original grid
   print_grid(game_grid)
 
+  while True:
+    xo = input("So who plays first? X or O\n").upper()
+    if xo == 'X' or xo == 'O':
+      break
+
   # Place X or O at chosen space and print new grid
   count = 0
-  xo = 'X'
   while (count < 9):
-    if xo == 'X':
-      choice = int(input("Place X at position: "))
-      game_grid = place_XO(game_grid, choice, xo)
-      if game_grid:
-        print_grid(game_grid)
-      else:
-        print("Position is occupied, try again!")
-      count += 1
-      xo = 'O'
+    while True:
+      try:
+        choice = int(input("Place X at position: "))
+      except ValueError:
+        print("Invalid Input! Enter a number between 1 and 9")
+        continue
 
-    elif xo == 'O':
-      choice = int(input("Place O at position: "))
-      game_grid = place_XO(game_grid, choice, xo)
-      if game_grid:
+      test_grid = place_XO(game_grid, choice, xo)   # Test grid used to test if place_XO returns a grid or 0
+
+      if test_grid != 0:        # if returned value is a grid then we later store it in game_grid else we dont
+        game_grid = test_grid
         print_grid(game_grid)
-      else:
-        print("Position is occupied, try again!")
-      count += 1
+        break
+
+    if winner(game_grid) != 0:
+      print("The winner is:", winner(game_grid))
+      break
+
+    if xo == 'X':
+      xo = 'O'
+    else:
       xo = 'X'
 
-  print_winner(game_grid)
+    count += 1      # Increment count by 1 after each iteration of while loop
+
+  if winner(game_grid) == 0:
+    print("It's a DRAW!")
+  print("Thank you for playing!")
 
 if __name__ == "__main__":
   main()
